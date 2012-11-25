65fe93481a9443b2e0b2f776aeeb7bc8554fa39a
xiaojue <designsor@gmail.com>
2012-11-25 22:13:39 +0800

    add chkconfig file

--- help.sh
+++ help.sh
@@ -3,13 +3,12 @@ cd '/home/tuer2.0/'
 cur_dir=$(pwd)
 #重启服务
 cd $cur_dir
-forever stop index.js
-forever start index.js
 #备份数据库
 date_now=`date +%Y%m%d%H%M`
 backmongodbFile=tuer$date_now.tar.gz
 cd $cur_dir/backup/
-mongodump -h 127.0.0.1 --port 10001 -d node-mongo-tuer -o my_mongodb_dump/
+/usr/bin/mongodump -h 127.0.0.1 --port 10001 -d node-mongo-tuer -o my_mongodb_dump/
 rm *.tar.gz
 tar czf $backmongodbFile my_mongodb_dump/
 rm my_mongodb_dump -rf
+service tuer restart
