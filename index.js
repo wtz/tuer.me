f0f79fd1c32f9a64cb68adc5d6b732b3ce39bcf9
xiaojue <designsor@gmail.com>
2013-01-20 20:36:09 +0800

    修改右侧顶部布局

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var tuer = require('./app');
 
 tuer.start({
-    rootdir:'/home/tuer2.0/',
+    rootdir:'/home/fuqiang/dev/tuerSource/',
     port:3000,
     mport:3030,
-    cookiepath:'tuer.me',
-    host:'www.tuer.me',
-    jshost:'js.tuer.me',
-    csshost:'css.tuer.me',
-    imagehost:'img.tuer.me',
+    cookiepath:'127.0.0.1',
+    host:'127.0.0.1:3000',
+    jshost:'127.0.0.1:3000',
+    csshost:'127.0.0.1:3000',
+    imagehost:'127.0.0.1:3000',
     dbname:'node-mongo-tuer',
     dbhost:'127.0.0.1',
     dbport:10001,
-    mhost:'m.tuer.me'
+    mhost:'127.0.0.1:3030'
 });
