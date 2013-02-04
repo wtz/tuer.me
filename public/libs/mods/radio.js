1413fa4e4c44d36575730e52ee4d2332d94ffcd4
xiaojue <designsor@gmail.com>
2013-02-04 17:00:02 +0800

    add domain

--- public/libs/mods/radio.js
+++ public/libs/mods/radio.js
@@ -1,4 +1,7 @@
 define(function(require, exports, module) {
+    
+   document.domain = 'tuer.me'; 
+
    var raidoHtml = '<div id="radio">\
                 <div class="radio-iframe"><iframe id="luoo-iframe" src="http://www.luoo.net/tuer/soundmanager/demo/page-player/radio.html" frameborder="no" scrolling="no" style="width:100%;height:auto;overflow:hidden;"></iframe></div>\
                 </div>';
