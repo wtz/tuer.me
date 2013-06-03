7ebb67fb1cac9f6dce64fdfac3ff65ae3a0ad8c5
xiaojue <designsor@gmail.com>
2013-06-03 12:51:29 +0800

    fix log

--- myreboot.sh
+++ myreboot.sh
@@ -2,7 +2,7 @@
 nc -w 10 -z 127.0.0.1 10001 > /dev/null 2>&1
 if [ $? -eq 0 ]
 then
-    echo mongodb status is ok
+    echo 'mongodb status is ok' > /home/tuer2.0/mongoStatus
 else
     killall node
     rm -rf /data/db/mongod.lock
