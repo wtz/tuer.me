8e5e2b33e02a82928c36f0ed1896163b9a6c16e0
xiaojue <designsor@gmail.com>
2013-06-03 13:35:44 +0800

    rm reboot

--- myreboot.sh
+++ myreboot.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+date_now=`date +%Y%m%d%H%M`
 nc -w 10 -z 127.0.0.1 10001 > /dev/null 2>&1
 if [ $? -eq 0 ]
 then
-    echo 'mongodb status is ok' > /home/tuer2.0/mongoStatus
+    echo $date_now : mongodb status is ok >> /home/tuer2.0/mongoStatus
 else
-    killall node
+    killall mongod
     rm -rf /data/db/mongod.lock
-    reboot
+    /usr/bin/mongod -dbpath=/data/db --fork --port 10001 --logpath=/data/log/mongodwork.log --logappend
 fi
