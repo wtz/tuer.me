735e3ec99034d2efc29d79200b99859515301cec
xiaojue <designsor@gmail.com>
2013-06-03 13:46:06 +0800

    fix >>

--- myreboot.sh
+++ myreboot.sh
@@ -3,7 +3,7 @@ date_now=`date +%Y%m%d%H%M`
 nc -w 10 -z 127.0.0.1 10001 > /dev/null 2>&1
 if [ $? -eq 0 ]
 then
-    echo $date_now : mongodb status is ok >> /home/tuer2.0/Status
+    echo $date_now : mongodb status is ok > /home/tuer2.0/Status
 else
     killall mongod
     rm -rf /data/db/mongod.lock
@@ -12,7 +12,7 @@ fi
 nc -w 10 -z 127.0.0.1 3000 > /dev/null 2>&1
 if [ $? -eq 0 ]
 then
-    echo $date_now : tuer status is ok >> /home/tuer2.0/Status
+    echo $date_now : tuer status is ok > /home/tuer2.0/Status
 else
     killall node 
     sh /home/tuer2.0/help.sh
