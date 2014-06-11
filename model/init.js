5b57d296c6ec52d4cd9f417e2857a2a4adac8b14
xiaojue <designsor@gmail.com>
2014-06-11 21:39:52 +0800

    fix package

--- model/init.js
+++ model/init.js
@@ -6,6 +6,7 @@ db.createCollection("tips");
 db.createCollection("notebooks");
 db.createCollection("todos");
 db.createCollection("apis");
+db.createCollection("images");
 db.notebooks.insert({name:"默认日记",owner:-1});
 db.users.insert({
     accounts:"admintest@tuer.me",
